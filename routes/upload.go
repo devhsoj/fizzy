b602d41bf93b115723ab570532a00e7d3a49e8a1
Joshua Benfield <josh@benfield.org>
2023-11-12 00:19:10 -0500

    added multiple file support

--- routes/upload.go
+++ routes/upload.go
@@ -12,33 +12,37 @@ import (
 )
 
 func UploadRoute(c *fiber.Ctx) error {
-	file, err := c.FormFile("file")
+	form, err := c.MultipartForm()
 
 	if err != nil {
 		log.Println(err)
 		return c.SendStatus(500)
 	}
 
+	files := form.File["files"]
+
 	// i really don't like having logic like this in a route, but since this is one of only two total routes, i can live with it :)
 
-	now := time.Now()
-	id := xxhash.Sum64String(fmt.Sprintf("%s-%d-%d", file.Filename, file.Size, now.UnixMilli()))
+	for _, file := range files {
+		now := time.Now()
+		id := xxhash.Sum64String(fmt.Sprintf("%s-%d-%d", file.Filename, file.Size, now.UnixMilli()))
 
-	upload := lib.Upload{
-		Id:           id,
-		Filename:     file.Filename,
-		Size:         uint64(file.Size),
-		DateUploaded: now,
-	}
+		upload := lib.Upload{
+			Id:           id,
+			Filename:     file.Filename,
+			Size:         uint64(file.Size),
+			DateUploaded: now,
+		}
 
-	if err := c.SaveFile(file, fmt.Sprintf("./static/%d", id)); err != nil {
-		log.Println(err)
-		return c.SendStatus(500)
-	}
+		if err := c.SaveFile(file, fmt.Sprintf("./static/%d", id)); err != nil {
+			log.Println(err)
+			return c.SendStatus(500)
+		}
 
-	if _, err = index.WriteUploadToIndex(&upload); err != nil {
-		log.Println(err)
-		return c.SendStatus(500)
+		if _, err = index.WriteUploadToIndex(&upload); err != nil {
+			log.Println(err)
+			return c.SendStatus(500)
+		}
 	}
 
 	return c.Redirect("/")
